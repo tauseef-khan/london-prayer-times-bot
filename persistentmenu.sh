2f5ef3cbdb37b52036495c895073664a039e52b5
tauseef-khan <tdakhan@gmail.com>
2017-06-29 17:40:02 +0100

    adds persistentmenu.sh

---
+++ persistentmenu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# persistentmenyu.sh: sets persistent menu
+# Execute once only
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "persistent_menu":[
+    {
+      "locale":"default",
+      "composer_input_disabled":false,
+      "call_to_actions":[
+        {
+          "type":"postback",
+          "title":"Menu",
+          "payload":"Menu Payload"
+        },
+        {
+          "type":"web_url",
+          "title":"Like LPT Bot",
+          "url":"https://www.facebook.com/London-Prayer-Times-Bot-139230819969675/",
+          "webview_height_ratio": "full",
+        },
+      ]
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=YOUR_ACCESS_TOKEN_HERE"
+    
