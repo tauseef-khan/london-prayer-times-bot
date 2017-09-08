a1c349bf691c23d64b6a9997e441fa18de9e2825
tauseef-khan <tdakhan@gmail.com>
2017-09-09 00:38:14 +0100

    d

--- sheets.py
+++ sheets.py
@@ -53,7 +53,7 @@ def construct_schedule():
     # read from excel sheet which has times in it
     # authentication
     scope = ['https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(StringIO.new(os.environ['GOOGLEAUTH']), scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(StringIO(os.environ['GOOGLEAUTH']), scope)
     gc = gspread.authorize(credentials)
 
     # Open a worksheet from spreadsheet with one shot
