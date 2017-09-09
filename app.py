6fb2977267ab8d5e60c93787d8888f69814ae837
tauseef-khan <tdakhan@gmail.com>
2017-09-10 00:34:24 +0100

    s

--- app.py
+++ app.py
@@ -75,7 +75,8 @@ def webhook():
                     try:
                         lat = list(location['attachments'])[0]
                         lat = list(lat['payload']['coordinates']['lat'])
-                        print("LAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaT", lat)
+                        log("LAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaT")
+                        log(lat)
 
                         lon = location['attachments'][0]['payload']['coordinates']['lon']
                     except:
