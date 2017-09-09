b6dadca033cd2b9f05cb9077cf963466b28baddb
tauseef-khan <tdakhan@gmail.com>
2017-09-09 23:11:02 +0100

    s

--- app.py
+++ app.py
@@ -39,30 +39,6 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    # call wit.ai method in utils.py
-                    ai_response = ai.wit_response(message_text)
-                    masjid, date = ai.extract_info(ai_response)
-
-                    if date != None:
-                        message = sheets.construct_schedule(date)
-                        send_message(sender_id, message)
-
-                    #message = sheets.construct_schedule()
-                    #send_message(sender_id, message)
-                    #send_message(sender_id, "This is where I am when user puts in their onw message...maybe.")
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
@@ -80,6 +56,24 @@ def webhook():
 
                     if (payload_text == "coordinates"):
                         send_message(sender_id, "Found location")
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    # call wit.ai method in utils.py
+                    ai_response = ai.wit_response(message_text)
+                    masjid, date = ai.extract_info(ai_response)
+
+                    if date != None:
+                        message = sheets.construct_schedule(date)
+                        send_message(sender_id, message)
+
+                    #message = sheets.construct_schedule()
+                    #send_message(sender_id, message)
+                    #send_message(sender_id, "This is where I am when user puts in their onw message...maybe.")
 
 
     return "ok", 200
