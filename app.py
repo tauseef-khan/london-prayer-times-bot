7d6a132e095fdb45b2e4f2fab08624f4100509a9
tauseef-khan <tdakhan@gmail.com>
2017-09-09 23:24:03 +0100

    s

--- app.py
+++ app.py
@@ -59,6 +59,9 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
+                    location = None
+                    message_text = None
+                    
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
 
